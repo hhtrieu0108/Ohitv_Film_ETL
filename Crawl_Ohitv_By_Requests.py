fd6c3ad24cd4265a321acaa9e6e0f0d734eabe62
Ngô Mỹ Quyền <ngomyquyen@MacBook-Air-cua-Ngo.local>
2024-06-12 12:50:27 +0700

    update function

--- Crawl_Ohitv_By_Requests.py
+++ Crawl_Ohitv_By_Requests.py
@@ -4,11 +4,13 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-def get_url():
-    url = "https://ohitv.net/"
+def get_soup(url):
     html = requests.get(url)
     soup = BeautifulSoup(html.text,'html.parser')
-
+    return soup
+def get_url():
+    url = "https://ohitv.net/"
+    soup = get_soup(url)
     kind_href = []
     kind_menu = soup.find_all('ul',class_='sub-menu')[1].find_all('a',href=True)
     kind_link = [link for link in kind_menu]
@@ -27,15 +29,13 @@ def crawl_title():
     title = []
     kind_href = get_url()
     for link in kind_href:
-        html = requests.get(link)
-        soup = BeautifulSoup(html.text,'html.parser')
+        soup = get_soup(link)
         data_html = soup.find('div',class_='items normal').find_all('div',class_='data')
         title_1 = [title_param.find('h3').text for title_param in data_html]
         title.extend(title_1)
         pages = get_page(soup=soup)
         for page in pages:
-            html = requests.get(page)
-            soup = BeautifulSoup(html.text,'html.parser')
+            soup = get_soup(page)
             data_html = soup.find('div',class_='items normal').find_all('div',class_='data')
             title_2 = [title_param.find('h3').text for title_param in data_html]
             title.extend(title_2)
@@ -45,15 +45,13 @@ def crawl_film_link():
     film_link = []
     kind_href = get_url()
     for link in kind_href:
-        html = requests.get(link)
-        soup = BeautifulSoup(html.text,'html.parser')
+        soup = get_soup(link)
         data_html = soup.find('div',class_='items normal').find_all('div',class_='data')
         film_link_1 = [link_param.find('h3').find('a',href=True)['href'] for link_param in data_html]
         film_link.extend(film_link_1)
         pages = get_page(soup=soup)
         for page in pages:
-            html = requests.get(page)
-            soup = BeautifulSoup(html.text,'html.parser')
+            soup = get_soup(page)
             data_html = soup.find('div',class_='items normal').find_all('div',class_='data')
             film_link_2 = [link_param.find('h3').find('a',href=True)['href'] for link_param in data_html]
             film_link.extend(film_link_2)
@@ -63,15 +61,13 @@ def crawl_date():
     date = []
     kind_href = get_url()
     for link in kind_href:
-        html = requests.get(link)
-        soup = BeautifulSoup(html.text,'html.parser')
+        soup = get_soup(link)
         data_html = soup.find('div',class_='items normal').find_all('div',class_='data')
         date_1 = [date_param.find('span').text for date_param in data_html]
         date.extend(date_1)
         pages = get_page(soup=soup)
         for page in pages:
-            html = requests.get(page)
-            soup = BeautifulSoup(html.text,'html.parser')
+            soup = get_soup(page)
             data_html = soup.find('div',class_='items normal').find_all('div',class_='data')
             date_2 = [date_param.find('span').text for date_param in data_html]
             date.extend(date_2)
@@ -81,15 +77,13 @@ def crawl_rating():
     rating = []
     kind_href = get_url()
     for link in kind_href:
-        html = requests.get(link)
-        soup = BeautifulSoup(html.text,'html.parser')
+        soup = get_soup(link)
         rating_data = soup.find_all('div',class_='rating')
         rating_1 = [rating_param.text for rating_param in rating_data]
         rating.extend(rating_1)
         pages = get_page(soup=soup)
         for page in pages:
-            html = requests.get(page)
-            soup = BeautifulSoup(html.text,'html.parser')
+            soup = get_soup(page)
             rating_data = soup.find_all('div',class_='rating')
             rating_2 = [rating_param.text for rating_param in rating_data]
             rating.extend(rating_2)
@@ -99,15 +93,13 @@ def crawl_quality():
     quality = []
     kind_href = get_url()
     for link in kind_href:
-        html = requests.get(link)
-        soup = BeautifulSoup(html.text,'html.parser')
+        soup = get_soup(link)
         quality_data = soup.find_all('div',class_='mepo')
         quality_1 = [quality_param.text for quality_param in quality_data]
         quality.extend(quality_1)
         pages = get_page(soup=soup)
         for page in pages:
-            html = requests.get(page)
-            soup = BeautifulSoup(html.text,'html.parser')
+            soup = get_soup(page)
             quality_data = soup.find_all('div',class_='mepo')
             quality_2 = [quality_param.text for quality_param in quality_data]
             quality.extend(quality_2)
@@ -117,16 +109,14 @@ def crawl_genre():
     genre = []
     kind_href = get_url()
     for link in kind_href:
-        html = requests.get(link)
-        soup = BeautifulSoup(html.text,'html.parser')
+        soup = get_soup(link)
         genre_data = soup.find_all('div',class_='mta')
         for genre_data_sub in genre_data:
             sub_type = [text.text for text in genre_data_sub]
             genre.append(sub_type)
         pages = get_page(soup=soup)
         for page in pages:
-            html = requests.get(page)
-            soup = BeautifulSoup(html.text,'html.parser')
+            soup = get_soup(page)
             genre_data = soup.find_all('div',class_='mta')
             for genre_data_sub in genre_data:
                 sub_type = [text.text for text in genre_data_sub]
@@ -137,15 +127,13 @@ def crawl_short_description():
     short_des = []
     kind_href = get_url()
     for link in kind_href:
-        html = requests.get(link)
-        soup = BeautifulSoup(html.text,'html.parser')
+        soup = get_soup(link)
         short_des_data = soup.find_all('div',class_='texto')
         short_des_1 = [short.text for short in short_des_data]
         short_des.extend(short_des_1)
         pages = get_page(soup=soup)
         for page in pages:
-            html = requests.get(page)
-            soup = BeautifulSoup(html.text,'html.parser')
+            soup = get_soup(page)
             short_des_data = soup.find_all('div',class_='texto')
             short_des_2 = [short.text for short in short_des_data]
             short_des.extend(short_des_2)
@@ -174,9 +162,9 @@ def load_data_base(df,username,password,host,database,table_name):
 if __name__ == "__main__":
     df = convert_to_dataframe()
     load_data_base(
-                    df=df,username='your_username',
-                    password='your_password',
-                    host='your_host',       
-                    database='your_database',
+                    df=df,username='postgres',
+                    password='304018',
+                    host='localhost',       
+                    database='ohitv',
                     table_name='ohitv_request'
                     )
